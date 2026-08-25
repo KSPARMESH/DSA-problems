a44417127dfaf5dceb55f2e33e7c92d37dc1c51f
Parmesh KS <89010022+KSPARMESH@users.noreply.github.com>
2026-08-25 16:36:48 +0530

    Time: 0 ms (100%), Space: 7.7 MB (77.17%) - LeetHub

---
+++ 0263-ugly-number/0263-ugly-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isUgly(int n) {
+        if(n==0){
+            return false;
+        }
+        int prime[3]={2,3,5};
+        for(int i=0;i<3;i++){
+            while(n%prime[i]==0){
+                n/=prime[i];
+            }
+        }
+        return n==1;
+    }
+};
